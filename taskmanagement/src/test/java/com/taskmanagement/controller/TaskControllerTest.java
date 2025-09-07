8013f5f84ca5ec9a1b444e4747f5645f71f539b1
AraveetiBharath13 <bharataraveeti46@gmail.com>
2025-09-07 17:33:47 +0530

    Adding test cases

--- taskmanagement/src/test/java/com/taskmanagement/controller/TaskControllerTest.java
+++ taskmanagement/src/test/java/com/taskmanagement/controller/TaskControllerTest.java
@@ -1,5 +1,49 @@
 package com.taskmanagement.controller;
 
+import com.taskmanagement.entity.Task;
+import com.taskmanagement.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.web.servlet.support.WebContentGenerator;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(TaskController.class)
+@AutoConfigureMockMvc
 public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private TaskService taskService;
+
+    @InjectMocks
+    private TaskController taskController;
+    @Autowired
+    private WebContentGenerator webContentGenerator;
+
+    @Test
+    public void   getAllTasksTest() throws Exception {
+        when(taskService.getAllTasks()).then((Answer<?>) List.of(Task.class));
+        mockMvc.perform(get("/tasks"))   // your endpoint here
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].taskName").value("Task One"));
+
+
+    }
 }
